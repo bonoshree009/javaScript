d34fcf1811c16e382e3ccf3ab8b4c5941ef398a6
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-20 21:33:23 +0600

    practice

---
+++ DOM/index.js
@@ -0,0 +1,30 @@
+console.log("hello from outside")
+//console.log(document)
+let values = document.getElementsByTagName("li")
+console.log(values)
+
+for(let value of values){
+    console.log(value.innerText)
+}
+
+console.log(document.getElementById("ok").innerText)
+
+let clas = document.getElementsByClassName("yes")
+console.log(clas)
+
+for(let v of clas){
+    console.log(v.innerText)
+}
+
+let q = document.querySelectorAll("#no h1")
+console.log(q)
+
+for(let all of q){
+    all.innerText ="nananana"
+    // console.log(all.innerText)
+    console.log(all)
+}
+let s = document.getElementById("one")
+s.style.backgroundColor ="red"
+s.classList.add("font")
+console.log(s)
