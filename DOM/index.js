d629d89daa0241298a6a90ba7530b0518cc1b530
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-20 22:44:09 +0600

    practice

--- DOM/index.js
+++ DOM/index.js
@@ -3,27 +3,27 @@ console.log("hello from outside")
 let values = document.getElementsByTagName("li")
 console.log(values)
 
-for(let value of values){
-    console.log(value.innerText)
-}
+// for(let value of values){
+//     console.log(value.innerText)
+// }
 
-console.log(document.getElementById("ok").innerText)
+// console.log(document.getElementById("ok").innerText)
 
-let clas = document.getElementsByClassName("yes")
-console.log(clas)
+// let clas = document.getElementsByClassName("yes")
+// console.log(clas)
 
-for(let v of clas){
-    console.log(v.innerText)
-}
+// for(let v of clas){
+//     console.log(v.innerText)
+// }
 
-let q = document.querySelectorAll("#no h1")
+let q = document.querySelectorAll("#no h1, h3")
 console.log(q)
 
-for(let all of q){
-    all.innerText ="nananana"
-    // console.log(all.innerText)
-    console.log(all)
-}
+// for(let all of q){
+//     all.innerText ="nananana"
+//     // console.log(all.innerText)
+//     console.log(all)
+// }
 let s = document.getElementById("one")
 s.style.backgroundColor ="red"
 s.classList.add("font")
