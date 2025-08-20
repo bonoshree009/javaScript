704988033b5ffc03f018fbb6b203d46daae18b84
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-21 00:59:52 +0600

    Dom

--- DOM/index.js
+++ DOM/index.js
@@ -16,8 +16,8 @@ console.log(values)
 //     console.log(v.innerText)
 // }
 
-let q = document.querySelectorAll("#no h1, h3")
-console.log(q)
+// let q = document.querySelectorAll("#no h1, h3")
+//console.log(q)
 
 // for(let all of q){
 //     all.innerText ="nananana"
@@ -25,6 +25,28 @@ console.log(q)
 //     console.log(all)
 // }
 let s = document.getElementById("one")
-s.style.backgroundColor ="red"
+s.style.backgroundColor ="orange"
+s.style.color = "white"
+s.style.padding = "20px"
 s.classList.add("font")
-console.log(s)
+//console.log(s)
+
+console.log(document.getElementById("one").childNodes[5].childNodes[1].parentNode)
+ let addlist = document.getElementById("add")
+let newchild = document.createElement("li")
+newchild.innerText = "hey i am new "
+console.log(newchild.innerText)
+addlist.appendChild(newchild)
+
+
+//another way
+
+let secondsec = document.getElementById("two")
+console.log(secondsec.innerText)
+secondsec.inneerHTML = ` <li> kaka babu </li>
+            <li> prof. shonku </li>
+            <li> shongkor </li>
+            <li> kiriti </li>
+            <li> mitin masi </li>
+`
+// two.appendChild(secondsec)
