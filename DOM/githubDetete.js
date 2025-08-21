a5edd1bd2aa8e870729e4b58187ca3cd9a93c922
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-22 00:56:28 +0600

    keyup

--- DOM/githubDetete.js
+++ DOM/githubDetete.js
@@ -1,1 +1,25 @@
-console.log('connected')
+// console.log('connected')
+document.getElementById("b").addEventListener('mouseenter',function(){
+   console.log("hi")
+
+ })
+document.getElementById("intext").addEventListener('keyup',function(){
+    //console.log('clicked')
+    let btndelete = document.getElementById("btn")
+    let text = document. getElementById("intext").value
+    //console.log(text)
+    if(text == 'delete'){
+        console.log("typeing")
+        btndelete.removeAttribute("disabled")
+        document.getElementById("btn").addEventListener('click',function(){
+            console.log("programe done")
+     } )
+        
+    }
+    else{
+        console.log("somethings else")
+        btndelete.setAttribute("disabled",true)
+    }
+    
+})
+
