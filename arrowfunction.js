12ddfb317574bafad7e08d69f6436c3cf35151b1
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-30 22:14:48 +0600

    spred oparator

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,4 +1,11 @@
-const sum =(a,b) => a+b
+// const sum =(a,b) => a+b
 
-const s = sum(2,9)
-console.log(s)
+// const s = sum(2,9)
+// console.log(s)
+
+// spred operator
+let arr = [10,20,30,443,209]
+let arr1 = [10,20,...arr]
+let arr3 = [ ...arr,...arr1]
+console.log(arr1)
+console.log(arr3)
