2940351a771f6d042d87bd7f8ba2e1100b7e0fc0
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-30 22:30:54 +0600

    object distructuring

--- arrowfunction.js
+++ arrowfunction.js
@@ -4,8 +4,26 @@
 // console.log(s)
 
 // spred operator
-let arr = [10,20,30,443,209]
-let arr1 = [10,20,...arr]
-let arr3 = [ ...arr,...arr1]
-console.log(arr1)
-console.log(arr3)
+// let arr = [10,20,30,443,209]
+// let arr1 = [10,20,...arr]
+// let arr3 = [ ...arr,...arr1]
+// console.log(arr1)
+// console.log(arr3)
+
+// ````````````````````````````object distructuring````````````````````````````
+
+
+const model = {
+    phn : "i",
+    series : "13 pro max",
+    price : 120000
+}
+console.log(model.series)
+
+const {phn,series} = {
+    phn : "sumsang",
+    series : "galaxy s 25",
+    price : 150000
+}
+console.log(phn,series)
+
