1a66133a7af7827462f34132a456412222192b1b
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-31 01:23:34 +0600

    optional chain

--- arrowfunction.js
+++ arrowfunction.js
@@ -3,6 +3,14 @@
 // const s = sum(2,9)
 // console.log(s)
 
+
+// Normal function
+// function greet1() { return "Hello"; }
+
+// // Arrow function
+// const greet = () => "Hello";
+// console.log(greet())
+
 // spred operator
 // let arr = [10,20,30,443,209]
 // let arr1 = [10,20,...arr]
@@ -31,17 +39,30 @@
 
 // ```````````````````````````````````object keys ,valus,delete,modify,freeze,seal```````````````````````````````````
 
-const model = {
-    phn : "i",
-    series : "13 pro max",
-    price : 120000
-}
-const keys = Object.keys(model) //sob key dekhabe
-//console.log(keys)
-const values = Object.values(model)
-//console.log(values) //sob valus show hobe
-delete model.phn
-model.year= 3030
-console.log(model)
-Object.freeze(model) //kono modity delete hobe na
-Object.seal(model) // shudu modity hobe
+// const model = {
+//     phn : "i",
+//     series : "13 pro max",
+//     price : 120000
+// }
+// const keys = Object.keys(model) //sob key dekhabe
+// //console.log(keys)
+// const values = Object.values(model)
+// //console.log(values) //sob valus show hobe
+// delete model.phn
+// model.year= 3030
+// console.log(model)
+// Object.freeze(model) //kono modity delete hobe na
+// Object.seal(model) // shudu modity hobe
+
+// ````````````````````optional chain`````````````````````````````````````
+// const model = {
+//     phn : {
+//          "type1":"android" ,
+//        "type2 ": "os"
+
+//           },
+//     series : "13 pro max",
+//     price : 120000
+// }
+// console.log(model.phn?)
+
