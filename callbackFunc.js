df21a5e5386fb8188b6a53a2a7f06e124d31c230
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-09-01 02:12:56 +0600

    callback function

---
+++ callbackFunc.js
@@ -0,0 +1,13 @@
+function biyesadi(callback,age){
+       if(age>18){
+    console.log(`${patro} abong ${patri} er biye hobe`)
+}
+else{
+    console.log("tader biye hobe na")
+}
+}
+function nam(){
+    patro = 'keru'
+    patri =" kumud"
+}
+biyesadi(nam(),20)
