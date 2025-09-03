889cc0e2c09873d9e00e403139c1f010803a80fb
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-09-04 02:45:00 +0600

    font add

--- json/todolist.js
+++ json/todolist.js
@@ -11,12 +11,14 @@ const displaydata =(posts)=>{
        // console.log(grid)
         const div = document.createElement("div")
         div.innerHTML =`<div class="rounded-2xl m-5 h-20 border-red-100 shadow-xl text-justify p-6 text-xl text-amber-500"> 
-          <h1>${post.completed} ${post.title}</h1></div>`
+          <h1>${post.completed} <i class="fa-solid fa-square-check"></i> <i class="fa-regular fa-square-check"></i> ${post.title}</h1></div>`
       grid.appendChild(div)
         
     });
 
 }
+
+
 // completed
 // : 
 // false
