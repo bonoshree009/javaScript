ffb8fa23a564450d2a30a3062ad8a99e08dd867a
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-09-11 01:52:15 +0600

    await

--- aysnc.js
+++ aysnc.js
@@ -1,5 +1,23 @@
-console.log("one")
+ async function  loaddata(){
+    console.log("one")
 console.log("two")
-fetch("").then(res=>res.json).then(() =>console.log("got data"))
+ await fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json).then(() =>console.log("got data"))
 console.log("four")
 console.log("five")
+}
+
+// loaddata()
+
+ const  loaddata2 =   async () =>{
+    console.log("one")
+console.log("two")
+try{
+ await fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json).then(() =>console.log("got data"))
+}
+catch(err){
+    console.log("error")
+}
+ console.log("four")
+console.log("five")
+}
+loaddata2()
